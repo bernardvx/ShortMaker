94bb8e8687efd1562c5d059b73c187e72c7fd402
Mackintosh <simonib20@gmail.com>
2024-03-30 01:47:44 +0100

    vid dims changing

--- Shorts/video_editing.py
+++ Shorts/video_editing.py
@@ -28,23 +28,21 @@ for i in os.listdir():
 
 for video in videos:
     file = f"{dir}{video}"
-
-    
     content = VideoFileClip(f'{file}')
     ad = VideoFileClip(ad)
     subs = f'{file}_en.srt'.replace('.mp4', '')
-    generator = lambda txt: TextClip(txt.upper(), font='Helvetica-Bold', fontsize=65, color='white', method='caption', 
-                                        stroke_color='black', stroke_width=1.5, align="center", size=(810, 1440))
+    generator = lambda txt: TextClip(txt.upper(), font='Helvetica-Bold', fontsize=72, color='white', method='caption', 
+                                        stroke_color='black', stroke_width=1.75, align="center", size=(810, 1440))
     srt_file = file_to_subtitles(subs)
     subtitles = SubtitlesClip(srt_file, generator)
+
     subtitles = subtitles.subclip(0, content.duration)
 
     ad=  ad.subclip(0, content.duration)
-    content = content.crop(x1=350, x2=1570) #remove 360px form each side of the width of the video
+    content = content.crop(x1=420, x2=1500) #remove 360px form each side of the width of the video
  
 
-    content = content.resize((1080, 1000))
-    ad = ad.resize((1080, 970))
+    content = content.resize((1080, 1012)) 
 
     def number_of_videos(content):
         return content.duration//30 - 1
@@ -53,12 +51,12 @@ for video in videos:
     vid_array = clips_array([[content],[ad]])
 
     while True:
-        title = content.filename.replace('.mp4', '')# its lol just for testing
+        title = content.filename.replace('.mp4', '')#remove the .mp4 on the name
         
         #edit clips with parts
         while i<number_of_videos(content):
-            final_short = CompositeVideoClip((vid_array, subtitles.set_position("center")), size=((1080, 1920))).subclip(b, e)
-            final_short.resize(width=1080, height=1920).write_videofile(f'{title}_Part_{i}.mp4', fps=24)     
+            final_short = CompositeVideoClip((vid_array, subtitles.set_position("center")), size=((1080, 1800))).subclip(b, e)
+            final_short.resize(width=1080, height=1800).write_videofile(f'{title}_Part_{i}.mp4', fps=24)    
             os.rename(f'{title}_Part_{i}.mp4', f'{title}_Part_{i}.mp4'.replace('Content_subs', 'Shorts'))
 
             b += 30
@@ -68,13 +66,14 @@ for video in videos:
             final_short = CompositeVideoClip((vid_array, subtitles.set_position("center")), size=((1080, 1920))).subclip(e, content.duration)
             final_short.resize(width=1080, height=1920).write_videofile(f'{title}_Part_{i}.mp4', fps=24)   
             os.rename(f'{title}_Part_{i}.mp4', f'{title}_Part_{i}.mp4'.replace('Content_subs', 'Shorts'))
+            
 
             i, b, e =0, 0, 30
             os.rename(f'{dir}{video}', f'{edited_dir}{video}')
 
             break
  
-        
+    
 
 
     print('done')
