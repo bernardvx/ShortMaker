9520a29a18cb40abe5c88fa73f868e0517a64329
Mackintosh <simonib20@gmail.com>
2024-06-03 20:15:00 +0200

    minor change to the title

--- uploader.py
+++ uploader.py
@@ -15,7 +15,7 @@ vids = {}
     
 for i in sorted(os.listdir()):
     if i.endswith('.mp4'):
-        vids[f'{i}'] = f"{i.replace('_', ' ').replace('.mp4', '')}. \nCheck out the link in bio for a suprise #GossipGirlFan #PLLAddict #SceneStealer #Fashionista #TVObsessed #DiscountCode"
+        vids[f'{i}'] = f"{i.replace('_', ' ').replace('.mp4', '')}.#GossipGirlFan #SceneStealer #TVObsessed  \nCheck out the link in bio for a suprise #DiscountCode"
 
 instagram_account_id = instagram_uploader.instagram_account_id
 access_token = instagram_uploader.access_token
@@ -26,14 +26,13 @@ uploaded = 0
 for link, caption in vids.items(): 
     #upload 5 shorts with .5h increments 
     while uploaded < 5:
-        #print(dirc, link, caption)
+        print(f'{dirc}{link}, {caption[:95]}')
         #upload video on youtube, make a better description and put emojis on the tittles
-        """
         try:
-            youtube_uploader.initialize_upload(youtube, file=f'{dirc}{link}', title=caption[80], description=caption, tags=None, category='22', privacyStatus='public')
+            youtube_uploader.initialize_upload(youtube, file=f'{dirc}{link}', title=caption[:95] , description=f'{caption} #Shopping', tags=None, category='22', privacyStatus='public')
         except HttpError as e:
             print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
-        """
+        
         #posts video on IG
         response_ig = instagram_uploader.upload_video_to_insta(video_url=f"{online_dir}{link}", caption=caption)
         print(f'{online_dir}{link}', caption)
